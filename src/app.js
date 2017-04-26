3099c3d6b355fdae8953de533b09459ffa2aa3e6
Helder Traci <helderdiin.work@gmail.com>
2017-04-26 11:16:30 -0300

    Adding more mock routes to test

--- src/app.js
+++ src/app.js
@@ -16,5 +16,32 @@ reachjs.init({
   }, {
     title: 'Notícias',
     description: 'Descrição da Notícias',
+  }, {
+    title: 'Lorem ipsum dolor sit amet',
+    description: 'consectetur adipiscing elit',
+  }, {
+    title: 'Donec dignissim erat eget neque rutrum',
+    description: 'quis eleifend ante consectetur',
+  }, {
+    title: 'Curabitur sagittis',
+    description: 'mi id augue aliquet auctor',
+  }, {
+    title: 'Cras et rhoncus mi',
+    description: 'Donec non odio sem',
+  }, {
+    title: 'Cras vitae nunc non turpis',
+    description: 'bibendum eleifend at sollicitudin turpis',
+  }, {
+    title: 'Morbi eros sem',
+    description: 'tristique a ornare in',
+  }, {
+    title: 'blandit at velit',
+    description: 'facilisis sed est sit amet',
+  }, {
+    title: 'Nunc quam nulla',
+    description: 'luctus blandit dolor',
+  }, {
+    title: 'Suspendisse commodo',
+    description: 'mauris purus, tempus sagittis massa faucibus sit amet',
   }],
 });
